712e3460bd2bdd6e0dbf4a32dd26dd6cb88432eb
’Siva <datasweeper@outlook.com>
2016-09-12 14:57:33 +0530

    log dir added

--- config.js
+++ config.js
@@ -1,13 +1,20 @@
+var fs = require('fs'), path = require('path');
+var LOG_DIR = path.dirname(require.main.filename) + "/log";
+
+
+if (!fs.existsSync(LOG_DIR)){
+    fs.mkdirSync(LOG_DIR);
+}
 
 config = {
 	"agent" : {
 		type : "file",
-		name: "agent.log",
+		name: LOG_DIR + "/agent.log",
 		size: 5
 	},
 	"server" : {
-		type : "console",
-		name : "server.log",
+		type : "file",
+		name : LOG_DIR + "/server.log",
 		size : 5
 	}
 }
--- test/simple-logger-test.js
+++ test/simple-logger-test.js
@@ -4,6 +4,5 @@ tap = require('tap');
 
 log = new logger('agent')
 log1 = new logger('server');
-
-tap.equal('agent.log', log.name, 'check if numbers still work')
-tap.equal('server.log', log1.name, 'check if numbers still work')
+tap.equal('file', log.type, 'check if numbers still work')
+tap.equal('file', log1.type, 'check if numbers still work')
